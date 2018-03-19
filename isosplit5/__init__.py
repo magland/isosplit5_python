e5f4db159eda302039d24b15b8af4c3d39907aaa
Jeremy Magland <jeremy.magland@gmail.com>
2018-03-19 11:14:24 -0400

    fortran order of numpy array

--- isosplit5/__init__.py
+++ isosplit5/__init__.py
@@ -2,7 +2,7 @@ import numpy as np
 import isosplit5_interface
 
 def isosplit5(X):
-	X=X.astype(np.float32,copy=False) #copies only if type changes
+	X=X.astype(np.float32,copy=False,order='F') #copies only if type changes, but note that we require fortran order, which is essential for the interface
 	M=X.shape[0]
 	N=X.shape[1]
 	labels_out=np.zeros([N]).astype(np.int32)
